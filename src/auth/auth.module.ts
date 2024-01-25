1502ebfa0b8091cb9d5cbf4d3a8ab8308ca31810
IuraCPersonal <iurie.cius.personal@gmail.com>
2024-01-25 16:44:31 +0200

    feat(auth): implement jwt strategy and login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: Number(
+            configService.getOrThrow<number>('JWT_EXPIRATION_TIME'),
+          ),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
