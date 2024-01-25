cad15a322a1d99363f96ca6c9e15a2ce86cfeaad
IuraCPersonal <iurie.cius.personal@gmail.com>
2024-01-25 18:05:57 +0200

    chore(dev): class-transformer and class-validator dependencies, and update local strategy and create-user.input.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
+  app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-
   const configService = app.get(ConfigService);
   await app.listen(configService.getOrThrow('PORT'));
 }
