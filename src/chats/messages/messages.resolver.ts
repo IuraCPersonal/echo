4b6c0915bc1536d811404a628533779ff3d13e5b
IuraCPersonal <iurie.cius.personal@gmail.com>
2024-01-27 00:15:05 +0200

    Add GraphQL subscriptions and pub mechanism

--- src/chats/messages/messages.resolver.ts
+++ src/chats/messages/messages.resolver.ts
@@ -1,16 +1,23 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { MessagesService } from './messages.service';
 import { Message } from './entities/message.entity';
-import { UseGuards } from '@nestjs/common';
+import { Inject, UseGuards } from '@nestjs/common';
 import { CreateMessageInput } from './dto/create-message.input';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { TokenPayload } from 'src/auth/interfaces/token-payload.interface';
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 import { GetMessagesArgs } from './dto/get-messages.args';
+import { PUB_SUB } from 'src/common/constants/injection-tokens';
+import { PubSub } from 'graphql-subscriptions';
+import { MESSAGE_CREATED } from './constants/pubsub-triggers';
+import { MessageCreatedArgs } from './dto/message-created.args';
 
 @Resolver(() => Message)
 export class MessagesResolver {
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(
+    private readonly messagesService: MessagesService,
+    @Inject(PUB_SUB) private readonly pubSub: PubSub,
+  ) {}
 
   @Mutation(() => Message)
   @UseGuards(GqlAuthGuard)
@@ -28,5 +35,18 @@ export class MessagesResolver {
     @CurrentUser() user: TokenPayload,
   ) {
     return this.messagesService.getMessages(getMessagesArgs, user._id);
+  }
+
+  @Subscription(() => Message, {
+    filter: (payload, variables) => {
+      // Explanation: We are filtering the subscription payload
+      // by the chatId. If the chatId of the payload matches
+      // the chatId of the subscription, the payload will be
+      // sent to the client.
+      return payload.messageCreated.chatId === variables.chatId;
+    },
+  })
+  messageCreated(@Args() _messageCreatedArgs: MessageCreatedArgs) {
+    return this.pubSub.asyncIterator(MESSAGE_CREATED);
   }
 }
