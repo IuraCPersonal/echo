212670c7d82cf7387869ec26d80bf9ddca8725e9
IuraCPersonal <iurie.cius.personal@gmail.com>
2024-01-26 19:19:02 +0200

    Refactor Chat component to include avatar and timestamp for each message

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { useGetChat } from "../../hooks/useGetChat";
 import SendIcon from "@mui/icons-material/Send";
 import {
+  Avatar,
   Box,
   Divider,
+  Grid,
   IconButton,
   InputBase,
   Paper,
   Stack,
+  Typography,
 } from "@mui/material";
 import { useCreateMessage } from "../../hooks/useCreateMessage";
 import { useGetMessages } from "../../hooks/useGetMessages";
@@ -18,10 +21,21 @@ const Chat: React.FC = () => {
   const chatId = params._id!;
 
   const [message, setMessage] = React.useState("");
+  const divRef = React.useRef<HTMLDivElement | null>(null);
+  const location = useLocation();
 
   const { data } = useGetChat({ _id: chatId });
   const { data: messages } = useGetMessages({ chatId });
   const [createMessage] = useCreateMessage(chatId);
+
+  const scrollToBottom = () => {
+    divRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  React.useEffect(() => {
+    setMessage("");
+    scrollToBottom();
+  }, [messages, location]);
 
   const handleCreateMessage = async () => {
     await createMessage({
@@ -34,6 +48,7 @@ const Chat: React.FC = () => {
     });
 
     setMessage("");
+    scrollToBottom();
   };
 
   return (
@@ -44,10 +59,32 @@ const Chat: React.FC = () => {
       }}
     >
       <h2>{data?.chat.name}</h2>
-      <Box sx={{}}>
+      <Box
+        sx={{
+          maxHeight: "70vh",
+          overflowY: "scroll",
+        }}
+      >
         {messages?.messages.map((message) => (
-          <p>{message.content}</p>
+          <Grid container alignItems="center" marginBottom="1rem">
+            <Grid item xs={3} md={1}>
+              <Avatar alt="" src="" sx={{ height: 52, width: 52 }} />
+            </Grid>
+            <Grid item xs={9} md={11}>
+              <Stack>
+                <Paper sx={{ width: "fit-content" }}>
+                  <Typography sx={{ p: "0.9rem" }}>
+                    {message.content}
+                  </Typography>
+                </Paper>
+                <Typography variant="caption" sx={{ marginLeft: "0.25rem" }}>
+                  {new Date(message.createdAt).toLocaleTimeString()}
+                </Typography>
+              </Stack>
+            </Grid>
+          </Grid>
         ))}
+        <div ref={divRef}></div>
       </Box>
       <Paper
         sx={{
